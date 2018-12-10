7c176faf9e4775885482e8a07ba5b097d4230d3a
João P <jonhdrift@gmail.com>
2018-12-10 21:26:51 +0000

    v15-Merged com o Goncalo

--- thaliaPrototipo/src/main/java/thalia/atec/thaliaPrototipo/Controller/RestPost.java
+++ thaliaPrototipo/src/main/java/thalia/atec/thaliaPrototipo/Controller/RestPost.java
@@ -1,6 +1,13 @@
 package thalia.atec.thaliaPrototipo.Controller;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -231,6 +238,204 @@ public class RestPost {
 		return null;
 
 	}
+	
+	
+	@GetMapping("/allposts")
+	public ResponseEntity<?> getPosts(@RequestParam("iduser") String idpost,@RequestParam("tipo") int tipo,@RequestParam("sizepost") int sizepost,@RequestParam("page") int page,@RequestParam("range") int range) {
+		String dateStart;
+		String dateStop;
+		String ano, mes, dia, hora, minuto, segundo;
+		int sizeposts, q, size, temp=0;
+		int userwatching = 0;
+		
+		ArrayList<Post> tpost = (ArrayList<Post>) ffpost.findAllposts();
+		ArrayList<Post> posts = new ArrayList<>();
+		
+		 Collections.sort(tpost, new Comparator<Post>() {
+             @Override
+             public int compare(Post o1, Post o2) {
+
+                 Date d1 = null;
+                 Date d2 = null;
+                 DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+                 try {
+                     d1 = df.parse(o1.getDate());
+                     d2 = df.parse(o2.getDate());
+
+                 } catch (ParseException e) {
+                     e.printStackTrace();
+                 }
+
+                 return d2.compareTo(d1);
+             }
+         });
+		 DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		 Date today = Calendar.getInstance().getTime();        
+		 String reportDate = df.format(today);
+		 for(Post p : tpost) {
+			 	
+			 	 dateStart = p.getDate();
+				 dateStop = reportDate;
+
+
+				Date d1 = null;
+				Date d2 = null;
+
+				try {
+					d1 = df.parse(dateStart);
+					d2 = df.parse(dateStop);
+
+					long diff = d2.getTime() - d1.getTime();
+
+					long diffSeconds = diff / 1000 % 60;
+					long diffMinutes = diff / (60 * 1000) % 60;
+					long diffHours = diff / (60 * 60 * 1000) % 24;
+					long diffDays = diff / (24 * 60 * 60 * 1000);
+					long  diffYear = diffDays/365;
+					long  diffMounth = diffDays/30;;
+					
+					/*System.out.print(diffYear + " year, ");
+					System.out.print(diffMounth + " mounth, ");
+					System.out.print(diffDays + " days, ");
+					System.out.print(diffHours + " hours, ");
+					System.out.print(diffMinutes + " minutes, ");
+					System.out.print(diffSeconds + " seconds.");*/
+					
+					ano = String.valueOf(diffYear);
+					mes = String.valueOf(diffMounth);						
+					dia = String.valueOf(diffDays);
+					hora = String.valueOf(diffHours);
+					minuto = String.valueOf(diffMinutes);
+					segundo = String.valueOf(diffSeconds);
+
+					
+					if(diffDays != 0) {
+						if(diffDays >= 30) {
+							if(diffDays >= 365) {
+								if(diffYear == 1) {
+									p.setDate(ano+" ano atrás");
+								}else {
+									p.setDate(ano+" anos atrás");
+								}
+							}else {
+								if(diffMounth == 1) {
+									p.setDate(mes+" mês atrás");
+								}else {
+									p.setDate(mes+" meses atrás");
+								}
+							}
+							
+						}else {
+							if(diffDays == 1) {
+								p.setDate(dia+" dia atrás");
+							}else {
+								p.setDate(dia+" dias atrás");
+							}
+						}
+					}else {
+						if(diffHours != 0) {
+							if(diffHours == 1) {
+								p.setDate(hora+" hora atrás");
+							}else {
+								p.setDate(hora+" horas atrás");
+							}
+						}else {
+							if(diffMinutes != 0) {
+								if(diffMinutes == 1) {
+									p.setDate(minuto+" minuto atrás");
+								}else {
+									p.setDate(minuto+" minutos atrás");
+								}
+							}else {
+								if(diffSeconds == 1) {
+									p.setDate(segundo+" segundo atrás");
+								}else {
+									p.setDate(segundo+" segundos atrás");
+								}
+							}
+						}
+					}
+
+				 } catch (Exception e) {
+					e.printStackTrace();
+				 }
+
+			  }
+		
+		 
+		 sizeposts = tpost.size();
+		 q = sizeposts - sizepost;
+		 if(sizeposts <= sizepost) {
+			 q = 0;
+		 }
+		 
+		 size = (q + (range*page));
+		 
+		if(tipo == 0) {
+			for(Post p:tpost) {
+				if(temp >= size && temp < size+range) {
+					for(User u: urep.findAll()) {
+						if(u.getId().compareTo(p.getIduser())==0) {
+							userwatching =u.getWatching().size();	
+						}
+					}
+					p.setUserwatched(userwatching);
+					posts.add(p);
+				}
+				temp +=1;
+			}
+		}else if(tipo == 1) {
+			for(Post p:tpost) {
+				if(p.getId().equals(idpost)) {
+					for(User u: urep.findAll()) {
+						if(u.getId().compareTo(p.getIduser())==0) {
+							userwatching =u.getWatching().size();	
+						}
+					}
+					p.setUserwatched(userwatching);
+					posts.add(p);
+				}
+			}
+		}
+		
+		return new ResponseEntity<>(posts,HttpStatus.OK);
+	}
+	
+
+
+	@GetMapping("/finduser")
+	public ResponseEntity<?> getuser(@RequestParam("iduser") String iduser) {
+		
+		ArrayList<User> users = new ArrayList<>();
+
+			for(User u:urep.findAll()) {
+				if(u.getId().equals(iduser)) {
+					users.add(u);
+				}
+			}
+		
+		return new ResponseEntity<>(users,HttpStatus.OK);
+	}
+
+
+@GetMapping("/getlikes")
+public ResponseEntity<?> getlikes(@RequestParam("id_post") String id_post) {
+	ArrayList<Post> posts = new ArrayList<>();
+		for(Post p:ffpost.findAllposts()) {
+			if(p.getId().equals(id_post)) {
+				posts.add(p);
+			}
+		}
+	return new ResponseEntity<>(posts,HttpStatus.OK);
+}
+
+@GetMapping("/sizeposts")
+public ResponseEntity<String> getSize(){
+	int size=0;
+	size = ffpost.findAllposts().size();
+
+	return new ResponseEntity<String>(""+size,HttpStatus.OK);
+}
 	
 	
 	
